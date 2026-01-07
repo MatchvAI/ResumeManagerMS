70f151a1f877a1e491eee70a779b02aae34d8c37
RohitPounikar <rohitpounikar111@gmail.com>
2026-01-07 16:04:26 +0530

    first commit

---
+++ src/main/java/com/resumeManagerMS/ResumeManagerMsApplication.java
@@ -0,0 +1,13 @@
+package com.resumeManagerMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeManagerMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeManagerMsApplication.class, args);
+	}
+
+}
